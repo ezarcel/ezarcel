a90796dd7808332f2c9be5e13f23fd472384553a
Adam Jiménez <adamjiva05yt@gmail.com>
2020-09-18 12:44:29 +0200

    Commit

--- safasp/main.js
+++ safasp/main.js
@@ -2,12 +2,10 @@ console.log('main.js');
 function importJS(url) {
 	fetch('https://raw.githubusercontent.com/ezarcel/ezarcel/master/safasp/' + url, fetchOptions).then(r => r.text())
 		.then(r => {
-			console.log(r);
 			const script = document.createElement('script');
-			script.innerText = r;
+			script.src = `data:text/plain,${encodeURIComponent(r)}`;
 			document.body.appendChild(script);
 		})
 };
 importJS('all.js');
 importJS(location.href.slice(location.href.indexOf('/students/') + ('/students/').length) + '.js');
-if (location.href.includes('/students/index.php')) importJS('index.php.js');
