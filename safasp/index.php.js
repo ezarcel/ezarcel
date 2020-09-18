88027e5d2d91dab832da563949d04d6ae6368cdc
Adam Jiménez <adamjiva05yt@gmail.com>
2020-09-18 12:30:05 +0200

    Commit

--- safasp/index.php.js
+++ safasp/index.php.js
@@ -1,1 +1,8 @@
-console.log('hola')
+console.log('index.php.js')
+window.addEventListener('DOMContentLoaded', () => {
+	const photoAlbums = document.querySelector('main > div:nth-child(3) > div:nth-child(6)')
+	const photoAlbumsLink = photoAlbums.querySelector('.links a')
+	photoAlbums.querySelector('.titol i').classList.replace('fa-list', 'fa-images')
+	photoAlbumsLink.href = '/students/albums_fotos.php'
+	photoAlbumsLink.innerText = 'Àlbums de fotografies'
+})
--- safasp/main.js
+++ safasp/main.js
@@ -1,1 +1,12 @@
-console.log('hola')
+console.log('main.js')
+function importJS(url) {
+	fetch('https://raw.githubusercontent.com/ezarcel/ezarcel/master/safasp/' + url).then(r => r.text())
+		.then(r => {
+			const script = document.createElement('script')
+			script.innerText = r
+			document.body.appendChild(script)
+		})
+}
+importJS('all.js')
+importJS(location.href.slice(location.href.indexOf('/students/')) + '.js')
+if (location.href.includes('/students/index.php')) importJS('index.php.js')
