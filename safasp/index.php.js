08c5cd6a10f4f1dc21a9bdf64b31004e755b0ce7
ezarcel <ezarcel05@gmail.com>
2020-10-19 19:11:46 +0200

    remove unnecessary files

--- safasp/index.php.js
+++
@@ -1,5 +0,0 @@
-const photoAlbums = document.querySelector('main > div:nth-child(3) > div:nth-child(6)')
-const photoAlbumsLink = photoAlbums.querySelector('.links a')
-photoAlbums.querySelector('.titol i').classList.replace('fa-list', 'fa-images')
-photoAlbumsLink.href = '/students/albums_fotos.php'
-photoAlbumsLink.innerText = 'Àlbums de fotografies'
